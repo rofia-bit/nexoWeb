58b694991a535e2ea8400bade8fe8bf1fa9c8269
Rofia <155486528+rofia-bit@users.noreply.github.com>
2025-09-12 02:57:41 +0100

    👍

---
+++ frontend/src/components/navbar/navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Navbar.css";
+import pizzaCursor from "../../assets/pizzacurso.png";
+
+export default function Navbar() {
+  return (
+    <header className="navbar">
+      {/* nexo logo */}
+      <div className="logo">nexo</div>
+
+      <nav className="nav-links">
+        <a href="#">Menu</a>
+        <a href="#">Offers</a>
+        <a href="#">Location</a>
+      </nav>
+
+      <button
+        className="order-btn"
+        style={{ cursor: `url(${pizzaCursor}) 16 16, pointer` }}
+      >
+        Order Now
+      </button>
+    </header>
+  );
+}
---
+++ frontend/src/pages/landingPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/navbar/navbar";
+import Hero from "../components/hero/hero";
+import Footer from "../components/footer/footer";
+
+export default function LandingPage() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Footer />
+    </div>
+  );
+}
