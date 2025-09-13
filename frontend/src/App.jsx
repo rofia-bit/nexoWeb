b979068de0e137f72f8f3e50f6b7941e87738ede
Rofia <155486528+rofia-bit@users.noreply.github.com>
2025-09-13 21:04:32 +0100

    👍

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Navbar from "./components/navbar/navbar";
 import Hero from "./components/hero/hero";
+import Login from "./components/login/login";
 
 export default function App() {
   return (
     <div>
-      <Navbar />
-      <Hero />
+      <Login />
     </div>
   );
 }
---
+++ frontend/src/components/login/login.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import "./login.css"; 
+
+function Login() {
+  // to make the vars dynamic >>>> usestate 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // function to handle the back later
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log("Login attempt:", { email, password });
+  };
+
+  const handleGuest = () => {
+    console.log("Continue as guest");
+    // later: navigate to menu
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-card">
+        {/* Logo hna */}
+        <h1 className="brand">NEXO PIZZA</h1>
+        <p className="tagline">pizza and tech blah blah blah</p>
+
+        <form onSubmit={handleLogin} className="form">
+          <input
+            type="email"
+            placeholder="Email address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+
+          <button type="submit" className="login-btn">
+            Login
+          </button>
+        </form>
+
+
+        <button onClick={handleGuest} className="guest-btn">
+          Continue as Guest
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
