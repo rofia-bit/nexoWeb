772ac08fa29166e49900bbbd991ff47a3b63e181
alixxxx-x <alix20041124@gmail.com>
2025-09-19 00:15:53 +0100

    new

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import './navbar.css';
+
+export default function Navbar(){
+    return (
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+        </nav>
+    );
+}
